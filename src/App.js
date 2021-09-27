2273b9daa084c7b1e39d498a22c90550d2ec07d6
Willem Ruan <ruan.nieu@gmail.com>
2021-09-27 08:50:48 +0200

    Dynamic quantity implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,139 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter ,Route, Switch } from "react-router-dom";
 import Homepage from "./Components/HomePage";
 import Navbar from "./Components/Navbar";
 import Products from "./Components/Products";
+import { useState } from "react";
 
 function App() {
+
+  const [count1, setCount1] = useState(1);
+  const [count2, setCount2] = useState(1);
+  const [count3, setCount3] = useState(1);
+  const [count4, setCount4] = useState(1);
+  const [count5, setCount5] = useState(1);
+  const [count6, setCount6] = useState(1);
+
+  const [cart, setCart] = useState({})
+
+  const price1 = 100;
+  const price2 = 110;
+  const price3 = 90;
+  const price4 = 65;
+  const price5 = 123;
+  const price6 = 30;
+
+  //SetState Functions
+
+    //Updating Product Count Functions
+
+  const updateCount1 = () =>{
+    setCount1(count1 + 1);
+  };
+
+  const updateCount2 = () =>{
+    setCount2(count2 + 1);
+  };
+
+  const updateCount3 = () =>{
+    setCount3(count3 + 1);
+  };
+
+  const updateCount4 = () =>{
+    setCount4(count4 + 1);
+  };
+
+  const updateCount5 = () =>{
+    setCount5(count5 + 1);
+  };
+
+  const updateCount6 = () =>{
+    setCount6(count6 + 1);
+  };
+
+  const negUpdateCount1 = () =>{
+    if(count1 > 1){
+      setCount1(count1 -1)
+    };
+  };
+
+  const negUpdateCount2 = () =>{
+    if(count2 > 1){
+      setCount2(count2 -1)
+    };
+  };
+
+  const negUpdateCount3 = () =>{
+    if(count3 > 1){
+      setCount3(count3 -1)
+    };
+  };
+
+  const negUpdateCount4 = () =>{
+    if(count4 > 1){
+      setCount4(count4 -1)
+    };
+  };
+
+  const negUpdateCount5 = () =>{
+    if(count5 > 1){
+      setCount5(count5 -1)
+    };
+  };
+
+  const negUpdateCount6 = () =>{
+    if(count6 > 1){
+      setCount6(count6 -1)
+    };
+  };
+
+  useEffect(() => {
+
+  });
+
   return (
     <div id="App">
       <BrowserRouter>
         <Navbar></Navbar>
         <Switch>
           <Route exact path = '/' component = {Homepage}/>
-          <Route path = '/Products' component = {Products} />
+          <Route path = '/Products' isAuthed={true}
+                render =  {(props) => <Products 
+                  price1 = {price1}
+                  price2 = {price2}
+                  price3 = {price3}
+                  price4 = {price4}
+                  price5 = {price5}
+                  price6 = {price6}
+
+                  name1 = "Hi"
+                  name2 = "Hi"
+                  name3 = "Hi"
+                  name4 = "Hi"
+                  name5 = "Hi"
+                  name6 = "Hi"
+
+                  counter1 = {count1}
+                  counter2 = {count2}
+                  counter3 = {count3}
+                  counter4 = {count4}
+                  counter5 = {count5}
+                  counter6 = {count6}
+
+                  updateCount1 = {updateCount1}
+                  updateCount2 = {updateCount2}
+                  updateCount3 = {updateCount3}
+                  updateCount4 = {updateCount4}
+                  updateCount5 = {updateCount5}
+                  updateCount6 = {updateCount6}
+
+                  NupdateCount1 = {negUpdateCount1}
+                  NupdateCount2 = {negUpdateCount2}
+                  NupdateCount3 = {negUpdateCount3}
+                  NupdateCount4 = {negUpdateCount4}
+                  NupdateCount5 = {negUpdateCount5}
+                  NupdateCount6 = {negUpdateCount6}
+                ></Products>}/>
         </Switch>
       </BrowserRouter>
     </div>
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -6,20 +6,67 @@ const Product = (props) => {
         <div id = 'Product'>
             <img src = {props.image} alt='product'/>
             <h4>{props.name}</h4>
+            <button id='minus'onClick = {props.NupdateCount} >-</button>
+            <p>{props.counter}</p>
+            <button id='plus' onClick = {props.updateCount} >+</button>
+            <p>{props.price*props.counter}</p>
+            <button id='add'>Add to Cart</button>
         </div>
     );
 };
 
-const Products = () => {
+const Products = (props) => {
 
     return(
         <div id='ProductDisplay'>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
-            <Product></Product>
+            <Product
+            image = {props.image1}
+            name = {props.name1}
+            counter = {props.counter1}
+            price = {props.price1}
+            updateCount = {props.updateCount1}
+            NupdateCount = {props.NupdateCount1}
+            ></Product>
+            <Product
+            image = {props.image2}
+            name = {props.name2}
+            counter = {props.counter2}
+            price = {props.price2}
+            updateCount = {props.updateCount2}
+            NupdateCount = {props.NupdateCount2}
+            ></Product>
+            <Product
+            image = {props.image3}
+            name = {props.name3}
+            counter = {props.counter3}
+            price = {props.price3}
+            updateCount = {props.updateCount3}
+            NupdateCount = {props.NupdateCount3}
+            ></Product>
+            <Product
+            image = {props.image4}
+            name = {props.name4}
+            counter = {props.counter4}
+            price = {props.price4}
+            updateCount = {props.updateCount4}
+            NupdateCount = {props.NupdateCount4}
+            ></Product>
+            <Product
+            image = {props.image5}
+            name = {props.name5}
+            counter = {props.counter5}
+            price = {props.price5}
+            updateCount = {props.updateCount5}
+            NupdateCount = {props.NupdateCount5}
+            ></Product>
+            <Product
+            image = {props.image6}
+            name = {props.name6}
+            counter = {props.counter6}
+            price = {props.price6}
+            updateCount = {props.updateCount6}
+            NupdateCount = {props.NupdateCount6}
+            ></Product>
         </div>
     );
 };
